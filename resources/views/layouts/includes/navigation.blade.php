f547fd3a8699d3001491c9bad4168f404b0848b4
Many <you@example.comtrymany84@gmail.com>
2021-06-04 23:46:20 +0700

    integrate bootstrap

--- resources/views/layouts/includes/navigation.blade.php
+++ resources/views/layouts/includes/navigation.blade.php
@@ -100,13 +100,13 @@
         <span></span>
         <span></span>
       </button>
-      <a class="navbar-brand text-brand" href="index.html">Estate<span class="color-b">Agency</span></a>
+      <a class="navbar-brand text-brand" href="/">Estate<span class="color-b">Agency</span></a>
 
       <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
         <ul class="navbar-nav">
 
           <li class="nav-item">
-            <a class="nav-link active" href="index.html">Home</a>
+            <a class="nav-link active" href="/">Home</a>
           </li>
 
           <li class="nav-item">
